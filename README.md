40d5c62111513476606d5cee79eaca1018f39e3b
mikejeter <mjeter45@yahoo.com>
2019-10-13 23:40:02 -0400

    Modified README.md, SearchForm.js

--- README.md
+++ README.md
@@ -28,6 +28,7 @@ Demonstrate your understanding of this Sprint's concepts by answering the follow
 - [ ]  Explain benefit(s) using `client-side routing`?
 
 > Answer:
+A client-side route happens when the route is handled internally by the JavaScript that is loaded on the page. When a user clicks on a link, the URL changes but the request to the server is prevented. The adjustment to the URL will result in a changed state of the application. The changed state will ultimately result in a different view of the webpage.
 
 ## Project Set Up
 
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
+import { Form, Field } from "formik";
+
 
 export default function SearchForm() {
+
+  const [users, setUsers] = useState([]);
+  
+    useEffect(() => {
+      if (props.status) {
+        setUsers([...users, props.status]);
+      }
+    }, [props.status]);
  
   return (
     <section className="search-form">
-     // Add a search form here
+     <Form>
+          <Field type="text" name="search" placeholder="Search" />
+          {props.touched.name && props.errors.name && (
+            <p className="error">{props.errors.name}</p>
+          )}
+          </Form>
     </section>
   );
 }
